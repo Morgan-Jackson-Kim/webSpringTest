3f906fd84b8ca6d75810d5df669a73d588e3729d
DESKTOP-JFG7H38\MorganKim <kjy2696@gmail.com>
2021-07-22 11:44:24 +0900

    [김지홍] lesson02/ test01 및 db연동

---
+++ src/main/java/com/morganj/spring/lesson02/bo/StoreBO.java
@@ -0,0 +1,21 @@
+package com.morganj.spring.lesson02.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.morganj.spring.lesson02.dao.StoreDAO;
+import com.morganj.spring.lesson02.model.Store;
+
+@Service
+public class StoreBO {
+	
+	@Autowired
+	private StoreDAO storeDAO;
+	
+	public List<Store> getStoreList(){
+		return storeDAO.selectStoreList();
+	}
+
+}
