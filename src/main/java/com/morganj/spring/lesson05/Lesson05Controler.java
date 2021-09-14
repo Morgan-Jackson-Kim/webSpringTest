afc74ff601fe725277cc14378bbd40172001c169
DESKTOP-JFG7H38\MorganKim <kjy2696@gmail.com>
2021-09-15 00:33:20 +0900

    [김지홍] test lesson05 lesson06 업로드

--- src/main/java/com/morganj/spring/lesson05/Lesson05Controler.java
+++ src/main/java/com/morganj/spring/lesson05/Lesson05Controler.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -197,7 +197,7 @@ public class Lesson05Controler {
 		return "lesson05/test05";
 	}
 	
-	@GetMapping("/text05/add_weather")
+	@PostMapping("/text05/add_weather")
 	public String addWeather(
 			@RequestParam("date")Date date,
 			@RequestParam("weather")String weather,
---
+++ src/main/java/com/morganj/spring/lesson06/bo/FavoriteBO.java
@@ -0,0 +1,24 @@
+package com.morganj.spring.lesson06.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.morganj.spring.lesson06.dao.FavoriteDAO;
+import com.morganj.spring.lesson06.model.Favorite;
+
+@Service
+public class FavoriteBO {
+	
+	@Autowired
+	private FavoriteDAO favoriteDAO;
+	
+	public int addFavorite(String title, String url) {
+		return favoriteDAO.insertFavorite(title, url);
+	}
+	
+	public List<Favorite> getFavoriteList(){
+		return favoriteDAO.selectFavoriteList();
+	}
+}
